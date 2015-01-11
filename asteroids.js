1b83fbae80ab7973298e06eb09e0930030113fe7
EpicPieGuy <ryanisaacg@gmail.com>
2015-01-11 08:52:41 -0500

    Added ability to shoot and destroy asteroids

--- asteroids.js
+++ asteroids.js
@@ -1,6 +1,14 @@
 
 
 
+function random(limit) {
+
+	return Math.random() * limit
+
+}
+
+
+
 function getSign(number) {
 
 	return Math.abs((number) / number)
@@ -85,19 +93,19 @@ function wrap(vector, width, height) {
 
 	var v = new Vector(vector.x, vector.y)
 
-	if(v.x > width)
+	if(v.x > width + 16)
 
 		v.x = v.x - width
 
-	if(v.x < 0)
+	if(v.x < -16)
 
 		v.x = width + v.x
 
-	if(v.y > height)
+	if(v.y > height + 16)
 
 		v.y = v.y - height
 
-	if(v.y < 0)
+	if(v.y < -16)
 
 		v.y = height + v.y
 
@@ -105,52 +113,6 @@ function wrap(vector, width, height) {
 
 }
 
-
-
-var Rectangle = function(x,  y,  width,  height){this.x=x
-this. y= y
-this. width= width
-this. height= height
-}
-
-
-
-function translateRect(rect, vector) {
-
-	return new Rectangle(rect.x + vector.x, rect.y + vector.y, rect.width, rect.height)
-
-}
-
-
-
-var Entity = function(pos,  vector,  rotation,  img){this.pos=pos
-this. vector= vector
-this. rotation= rotation
-this. img= img
-}
-
-g = getGraphics("canvas")
-
-
-
-player = new Entity(new Vector(0, 0), new Vector(0, 0), 0, getImage("images\\ship.png"))
-
-
-
-
-function drawEntity(graphics, entity) {
-
-	graphics.translate(entity.pos.x, entity.pos.y)
-
-	graphics.rotate(entity.rotation * Math.PI / 180)
-
-	graphics.drawImage(entity.img, -entity.img.width / 2, -entity.img.height / 2)
-
-	graphics.rotate(-entity.rotation * Math.PI / 180)
-
-	graphics.translate(-entity.pos.x, -entity.pos.y)
-
-}
 
 
 function limit(vector, amt) {
@@ -179,9 +141,75 @@ function limit(vector, amt) {
 
 
 
+function inArea(point, area, width, height) {
+
+	return (point.x >= area.x && point.y >= area.y) &&
+
+		(point.x <= area.x + width && point.y <= area.y + height)
+
+}
+
+
+var Entity = function(pos,  vector,  rotation,  img){this.pos=pos
+this. vector= vector
+this. rotation= rotation
+this. img= img
+}
+
+g = getGraphics("canvas")
+
+
+var shipTex, flareTex;
+
+shipTex = getImage("images\\ship.png")
+
+flareTex = getImage("images\\flare.png")
+
+asteroidTex = getImage("images\\asteroids.png")
+
+
+player = new Entity(new Vector(0, 0), new Vector(0, 0), 0, shipTex)
+
+player.delay = 0
+
+asteroids = []
+
+for(var i = 0; i < 3; i++) {
+
+	asteroids.push(new Entity(new Vector(random(960), random(540)), new Vector(random(2) - 1, random(2) - 1),0,asteroidTex))
+
+}
+
+bullets = []
+
+
+
+function drawEntity(graphics, entity) {
+
+	graphics.translate(entity.pos.x, entity.pos.y)
+
+	graphics.rotate(entity.rotation * Math.PI / 180)
+
+	graphics.drawImage(entity.img, -entity.img.width / 2, -entity.img.height / 2)
+
+	graphics.rotate(-entity.rotation * Math.PI / 180)
+
+	graphics.translate(-entity.pos.x, -entity.pos.y)
+
+}
+
+
+
 function step() {
 
+	
 	var WIDTH = 960, HEIGHT = 540
+
+	var accel = false
+
+	if(player.delay > 0)
+
+		player.delay -= 1
 
 	if(Keyboard["37"])
 
@@ -191,28 +219,79 @@ function step() {
 
 		player.rotation += 4
 
-	if(Keyboard["38"]) 
+	if(accel = Keyboard["38"]) 
 
 		player.vector = addVector(player.vector, angleToVector(player.rotation, 0.1))
+
+	if(Keyboard["32"] && player.delay <= 0) {
+
+		bullets.push(new Entity(player.pos, angleToVector(player.rotation, 5), 0, null))
+
+		player.delay = 20
+
+	}
 
 	player.pos = wrap(addVector(player.pos, player.vector), WIDTH, HEIGHT)
 
 	player.vector = limit(player.vector, 5)
 
+	for(var i = 0; i < bullets.length; i++) {
+
+		bullets[i].pos = wrap(addVector(bullets[i].pos, bullets[i].vector, WIDTH, HEIGHT))
+
+		if(bullets[i].pos.x < 0 || bullets[i].pos.x > WIDTH || bullets[i].pos.y < 0 || bullets[i].pos.y > HEIGHT)
+
+			bullets.splice(i, 1)
+
+	}
+
+	for(var j = 0; j < asteroids.length; j++) {
+
+		asteroids[j].pos = wrap(addVector(asteroids[j].pos, asteroids[j].vector), WIDTH, HEIGHT)
+
+		for(var i = 0; i < bullets.length; i++) {
+
+			if(inArea(bullets[i].pos, asteroids[j].pos, asteroids[j].img.width, asteroids[j].img.height)) {
+
+				asteroids.splice(j, 1)
+
+				bullets.splice(i, 1)
+
+				
+
+			}
+
+		}
+
+	}
+
 	
 
-	g.clearRect(0, 0, 960, 540)
+	
+	g.clearRect(0, 0, 970, 560)
 
-	g.fillStyle = "#FFFFFF"
+	player.img = shipTex
 
 	drawEntity(g, player)
 
+	if(accel){
+
+		player.img = flareTex
+
+		drawEntity(g, player)
+
+	}
+
+	for(var i = 0; i < asteroids.length; i++)
+
+		drawEntity(g, asteroids[i])
+
+	g.fillStyle = "#FFFFFF"
+
+	for(var i = 0; i < bullets.length; i++) 
+
+		g.fillRect(bullets[i].pos.x - 1, bullets[i].pos.y - 1, 2, 2)
+
 }
 
-function main() {
-
-	setInterval(step, 1000 / 60)
-
-}
-
-main()
+setInterval(step, 1000 / 60)
